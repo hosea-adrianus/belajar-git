ded645b365d9bf03709eb5577848b2754d29f914
Mhus <mhusaisyah@gmail.com>
2023-04-02 10:40:42 +0700

    Registration Service

--- src/main/java/com/mii/registration/login/security/registration/RegistrationService.java
+++ src/main/java/com/mii/registration/login/security/registration/RegistrationService.java
@@ -3,12 +3,30 @@ package com.mii.registration.login.security.registration;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import com.mii.registration.login.security.appuser.AppUser;
+import com.mii.registration.login.security.appuser.AppUserRole;
+import com.mii.registration.login.security.appuser.AppUserService;
+
 @Service
 @AllArgsConstructor
 public class RegistrationService {
-
+    private final AppUserService appUserService;
+    private EmailValidator emailValidator;
+    
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid!");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 
 
